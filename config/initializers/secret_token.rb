4ce7a4adf9b97e407846d3cf5c499b4e1a301642
P Elkington <pjelk0401@bigpond.com>
2013-09-23 11:26:14 +1000

    Created a Rails app and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moosepage::Application.config.secret_token = '02a258774b1c8ef68a9da8653305864305f27146f119d40b14bcb715a0c662400d0003da681eac90c5c7301540c9072f9df9e94dd857989a7de2723cc15d805e'
